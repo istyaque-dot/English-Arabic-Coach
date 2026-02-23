ec23a0ef8c103d14fb730f597f5e9e35757506fe
istyaque-dot <istyaque@gmail.com>
2026-02-23 08:22:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ try:
     API_KEY = st.secrets["GOOGLE_API_KEY"]
     genai.configure(api_key=API_KEY)
     # Latest model use kar rahe hain taaki NotFound error na aaye
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 except Exception as e:
     st.error(f"Setup Error: {e}")
 
